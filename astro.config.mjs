30d7facd0a3ec501199d2d63fe9170f63f7065fb
AMichelas <120266869+AMichelas@users.noreply.github.com>
2023-11-03 16:57:38 +0100

    maj

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://alexisimichelas.github.io/Portfolio',
+    site: 'https://alexisimichelas.github.io',
 });
